a390532e1d73bc0676a4247781b8aec7d88ddb32
g00hyun <rhkdgus1417@hufs.ac.kr>
2024-11-16 14:36:26 +0900

    :sparkles: [Feat] 이력서 및 채용공고 업로드 page

--- fe/src/App.js
+++ fe/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import { Routes, Route, Link } from 'react-router-dom';
 import VideoRecorder from './VideoRecorder';
+import FileUploadPage from './sendResume';
 
 function App() {
   return (
     <Routes>
-      <Route path="" element={<VideoRecorder />} />
+      <Route path="/send-response" element={<VideoRecorder />} />
+      <Route path="/send-resume" element={<FileUploadPage />} />
     </Routes>
   );
 }
