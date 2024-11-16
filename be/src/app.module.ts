f6ebc7468e06fdcd14696ebc3cf6c861508e3dee
g00hyun <rhkdgus1417@hufs.ac.kr>
2024-11-16 16:27:47 +0900

    [Feat] 인터뷰 영상 get api pdf api 구현

--- be/src/app.module.ts
+++ be/src/app.module.ts
@@ -3,9 +3,13 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { MLModule } from './ml/ml.module';
 import { VideoModule } from './video/video.module';
+import { MulterModule } from '@nestjs/platform-express';
 
 @Module({
   imports: [
+    MulterModule.register({
+      dest: './uploads',
+    }),
     ConfigModule.forRoot({
       isGlobal: true,
     }),
